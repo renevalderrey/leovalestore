99b5ca406762416d930fbc0f204d81bc3656a888
René <107271569+renevalderrey@users.noreply.github.com>
2024-02-25 00:33:39 -0300

    provider routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import Home from "./Pages/Home";
+import Services from "./Pages/Services";
+import SiteJob from "./Pages/SiteJob";
+import Inspo from "./Pages/Inspo";
+import Promotions from "./Pages/Promotions";
+import Information from "./Pages/Information";
+import Contact from "./Pages/Contact";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/servicios",
+    element: <Services />,
+  },
+  {
+    path: "/sitiodetrabajo",
+    element: <SiteJob />,
+  },
+  {
+    path: "/inspo",
+    element: <Inspo />,
+  },
+  {
+    path: "/promociones",
+    element: <Promotions />,
+  },
+  {
+    path: "/informacion",
+    element: <Information />,
+  },
+  {
+    path: "/ubicacionycontacto",
+    element: <Contact />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider route={router}>
+      <App />
+    </RouterProvider>
+  </React.StrictMode>
+);
