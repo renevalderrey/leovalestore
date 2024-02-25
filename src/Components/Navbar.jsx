1576ea43aff13cc463353400a132c7b5b403cbf1
René <107271569+renevalderrey@users.noreply.github.com>
2024-02-25 01:08:49 -0300

    rutas modificadas

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,27 +5,27 @@ import "../Styles/Navbar.css";
 const Navbar = () => {
   return (
     <nav>
-      <a href="#/">
+      <a href="/">
         <img src={logo} className="logo" />
       </a>
       <ul>
         <li>
-          <a href="#/servicios">SERVICIOS</a>
+          <a href="/servicios">SERVICIOS</a>
         </li>
         <li>
-          <a href="#/sitiodetrabajo">LUGAR DE TRABAJO</a>
+          <a href="/sitiodetrabajo">LUGAR DE TRABAJO</a>
         </li>
         <li>
-          <a href="#/inspo">INSPO</a>
+          <a href="/inspo">INSPO</a>
         </li>
         <li>
-          <a href="#/promociones">PROMOCIONES</a>
+          <a href="/promociones">PROMOCIONES</a>
         </li>
         <li>
-          <a href="#/informacion">INFORMACIÓN</a>
+          <a href="/informacion">INFORMACIÓN</a>
         </li>
         <li>
-          <a href="#/ubicacionycontacto">UBICACIÓN Y CONTACTO</a>
+          <a href="/ubicacionycontacto">UBICACIÓN Y CONTACTO</a>
         </li>
       </ul>
     </nav>
