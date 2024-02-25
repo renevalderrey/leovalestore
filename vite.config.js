18ecae711c9ef3ebb34299f15e1c5b5e767f40ce
René <107271569+renevalderrey@users.noreply.github.com>
2024-02-25 00:56:33 -0300

    act

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://renevalderrey.github.io/leovalestore/",
+  base: "https://renevalderrey.github.io/leovalestore",
 });
