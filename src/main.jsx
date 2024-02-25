d3103a0d2d8818150ac2e933d4aa263e8d8d6287
René <107271569+renevalderrey@users.noreply.github.com>
2024-02-25 14:12:16 -0300

    rutas navbar y main

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,50 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
+import Home from "./Pages/Home";
+import Services from "./Pages/Services";
+import SiteJob from "./Pages/SiteJob";
+import Inspo from "./Pages/Inspo";
+import Promotions from "./Pages/Promotions";
+import Information from "./Pages/Information";
+import Contact from "./Pages/Contact";
+
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/servicios",
+    element: <Services />,
+  },
+  {
+    path: "/sitiodetrabajo",
+    element: <SiteJob />,
+  },
+  {
+    path: "/inspo",
+    element: <Inspo />,
+  },
+  {
+    path: "/promociones",
+    element: <Promotions />,
+  },
+  {
+    path: "/informacion",
+    element: <Information />,
+  },
+  {
+    path: "/ubicacionycontacto",
+    element: <Contact />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <RouterProvider router={router}>
       <App />
+    </RouterProvider>
   </React.StrictMode>
 );
